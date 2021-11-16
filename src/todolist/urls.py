65996eb6857ed5fb4a4f9cb01fa552458985214b
Gabriel <storkdeveloper@gmail.com>
2021-11-16 12:58:22 -0300

    admin url removed

--- src/todolist/urls.py
+++ src/todolist/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
     path('user/', include('users.urls')),
 ]

7cf39cc566dbeba97462052c69f7d361d9f10dd7
Gabriel <storkdeveloper@gmail.com>
2022-02-10 10:14:50 -0300

    updating primary key type in urls

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.tasks, name='tasks'),
     path('tasks/add/', views.tasks_add, name='tasks_add'),
-    path('tasks/edit/<int:pk>/', views.tasks_edit, name='tasks_edit'),
-    path('tasks/delete/<int:pk>/', views.tasks_delete, name='tasks_delete'),
+    path('tasks/edit/<slug:pk>/', views.tasks_edit, name='tasks_edit'),
+    path('tasks/delete/<slug:pk>/', views.tasks_delete, name='tasks_delete'),
 ]

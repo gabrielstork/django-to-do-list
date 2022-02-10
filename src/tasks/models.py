d80e983d62998cfed8528f5c546795fd36e66ff5
Gabriel <storkdeveloper@gmail.com>
2022-02-10 10:13:17 -0300

    new custom primary key field

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 
 class Task(models.Model):
+    task_id = models.CharField(max_length=8, primary_key=True, unique=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
@@ -11,4 +12,4 @@ class Task(models.Model):
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return f'{self.user} - {self.title}'
